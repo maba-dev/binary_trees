899a937ebb1375a7ccd738127811ad1d587a26aa
maba-dev <cherif.ba@hotmail.fr>
2022-03-21 17:22:36 +0100

    task 0  a function that creates a binary tree node

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+/**
+ * @brief 
+ * 
+ */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+    binary_tree_t *racine = malloc(sizeof(*racine));
+    if (racine == NULL)
+        return NULL;
+    racine->n = value;
+    racine->parent = parent;
+    racine->left = NULL;
+    racine->right = NULL;
+    return racine;
+}
