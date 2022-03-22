556e2aa6edd63314daf893d1096c35397faf21e7
maba-dev <cherif.ba@hotmail.fr>
2022-03-22 14:32:08 +0100

    task 7 : a function that goes through a binary tree using in-order traversal

---
+++ 7-binary_tree_inorder.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+/**
+ * @brief 
+ * 
+ */
+
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+    if (tree == NULL)
+        return;
+    if (tree->left)
+        binary_tree_inorder(tree->left, (*func));
+    func(tree->n);
+    if (tree->right)
+        binary_tree_inorder(tree->right, (*func));
+}
