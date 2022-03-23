a00829b8d8bc176134cf6544f958d4be9648efc3
maba-dev <cherif.ba@hotmail.fr>
2022-03-23 21:39:29 +0100

    task 2 retry

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -19,15 +19,16 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	right_child->n = value;
 	right_child->parent = parent;
 
-	if (parent->left != NULL)
+	if (parent->right != NULL)
 	{
-		right_child->left = parent->right;
-		right_child->left->parent = right_child;
+		right_child->right= parent->right;
+		right_child->right->parent = right_child;
 	}
 	else
 		right_child->right = NULL;
 
-	parent->left = right_child;
+	parent->right = right_child;
 	right_child->left = NULL;
 	return (right_child);
 }
+
