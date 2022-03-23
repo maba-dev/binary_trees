ac3e60df31d0f58b781073a3546e109578971aea
maba-dev <cherif.ba@hotmail.fr>
2022-03-23 15:02:09 +0100

    task 14: a function that measures the balance factor of a binary tree

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,35 @@
+#include "binary_trees.h"
+
+/**
+ * @brief 
+ * 
+ */
+
+int tree_height(const binary_tree_t *tree)
+{
+    if (tree == NULL )
+        return 0;
+    if (tree->left == NULL && tree->right == NULL)
+        return 1;
+
+    int left_height = tree_height(tree->left);
+    int right_height = tree_height(tree->right);
+
+    if (left_height > right_height)
+        return (left_height) + 1;
+    else
+        return (right_height) + 1;
+}
+
+
+int binary_tree_balance(const binary_tree_t *tree)
+{
+    int left_height;
+    int right_height;
+    if (tree == NULL)
+        return 0;
+    left_height = tree_height(tree->left);
+    right_height = tree_height(tree->right);
+
+    return left_height - right_height;
+}
