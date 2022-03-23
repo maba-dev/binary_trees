60b65e8f55067db5aa82e2fd9a0592d4edf0b435
maba-dev <cherif.ba@hotmail.fr>
2022-03-23 15:52:16 +0100

    task 1: betty

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -13,12 +13,9 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	binary_tree_t *left_child = binary_tree_node(parent, value);
 
 
-	if (parent == NULL)
-		return (NULL);
-	if (left_child == NULL)
+	if (parent == NULL || left_child == NULL)
 		return (NULL);
 	left_child->left = parent->left;
-
 	if (left_child->left != NULL)
 		left_child->left->parent = left_child;
 	parent->left = left_child;
