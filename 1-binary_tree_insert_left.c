093f5c798d38d1f0496cdd753471274915a55900
maba-dev <cherif.ba@hotmail.fr>
2022-03-23 21:11:58 +0100

    task 1 retry

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -15,10 +15,19 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	if (parent == NULL || left_child == NULL)
 		return (NULL);
+
 	left_child->n = value;
-	left_child->left = parent->left;
-	if (left_child->left != NULL)
+	left_child->parent = parent;
+	left_child->right = NULL;
+
+	if (parent->left != NULL)
+	{
+		left_child->left = parent->left;
 		left_child->left->parent = left_child;
+	}
+	else
+		left_child->left = NULL;
 	parent->left = left_child;
+	
 	return (left_child);
 }
