9cbf6284e0d53a5ad1f1d3ee03aef698f2450df6
maba-dev <cherif.ba@hotmail.fr>
2022-03-24 00:36:03 +0100

    betty

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,16 +1,17 @@
 #include "binary_trees.h"
 /**
- * @brief 
- * 
+ * binary_tree_is_full -  a function that checks if a binary tree is full
+ * @tree: a binary tree
+ * Return: true if the binary tree is full
  */
 
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return (0);
-    if (tree->left == NULL && tree->right == NULL)
+	if (tree == NULL)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
 		return (1);
-    if (tree->right != NULL && tree->left != NULL)
-        return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
-    return (0);
+	if (tree->right != NULL && tree->left != NULL)
+		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+	return (0);
 }
--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -9,23 +9,11 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
 	binary_tree_t *sibling;
 
-	if (node  == NULL || node->parent == NULL)
+	if (node == NULL || node->parent == NULL)
 		return (NULL);
 	if (node == node->parent->left)
 		sibling = node->parent->right;
 	else
 		sibling = node->parent->left;
 	return (sibling);
-}
-
-/**
- * binary_tree_uncle - a function that finds the uncle of a node
- * @node: The node to binary trees
- * Return: The binary tree uncle
- */
-binary_tree_t *binary_tree_uncle(binary_tree_t *node)
-{
-	if (node == NULL)
-		return (NULL);
-	return (binary_tree_sibling(node->parent));
 }
