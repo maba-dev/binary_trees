931b921a10af47306e9ab7328577a10e5b8eb9a0
maba-dev <cherif.ba@hotmail.fr>
2022-03-23 16:30:17 +0100

    retry task 10

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,14 +1,15 @@
 #include "binary_trees.h"
 
 /**
- * @brief 
+ *
  * 
  */
 
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
+    size_t depth = 0;
     if (tree == NULL || tree->parent == NULL)
         return 0;
-    size_t depth = binary_tree_depth(tree->parent);
-    return depth + 1;
+    depth = binary_tree_depth(tree->parent) + 1;
+    return (depth);
 }
