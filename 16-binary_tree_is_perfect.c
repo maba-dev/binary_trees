4b0dad0d41be89cb068d5f21b2625d0c9c7c2593
maba-dev <cherif.ba@hotmail.fr>
2022-03-24 00:59:04 +0100

    correct betty

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -63,15 +63,16 @@ int binary_tree_is_full(const binary_tree_t *tree)
 }
 
 /**
- * @brief 
- * 
+ * binary_tree_is_perfect - checks if a binary tree is perfect
+ * @tree: a binary tree
+ * Return: true if the tree is perfect
  */
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return (0);
-    if (binary_tree_balance(tree) == 0 && binary_tree_is_full(tree) == 1)
-        return (1);
-    return (0);
+	if (tree == NULL)
+		return (0);
+	if (binary_tree_balance(tree) == 0 && binary_tree_is_full(tree) == 1)
+		return (1);
+	return (0);
 }
